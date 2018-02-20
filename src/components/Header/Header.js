f978b5b2d0d8d2c559128ac2e0794393bac1011b
Melodie Chi <chzwzrd@chzwz-2.local>
2018-02-20 13:46:31 -0800

    working header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar">
+                    <ul>
+                        <li className="brand"><a href="/">Clicky Game</a></li>
+                        <li>Click on an image to begin!</li>
+                        <li>Score: 0 | Top Score: 0</li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    };
+};
+
+export default Header;
